348ece9a22844b41da3548c78b2fb19afd19f54a
kabutor <43006263+kabutor@users.noreply.github.com>
2020-09-19 20:11:15 +0200

    Update decrypt_PVK.ps1

--- decrypt_PVK.ps1
+++ decrypt_PVK.ps1
@@ -2,7 +2,7 @@
 
 Get-ChildItem ".\RSA\S-1-5-21-788893716-389553871-1612069284-1001\"  | 
 Foreach-Object {
-    $pass = "USER_PASS"
+    $pass = "NEED TO TYPE IN USER_PASS"
 	#Write-Host $_.FullName
     $first_step = "mimikatz.exe `"dpapi::capi /in:" + $_.FullName + "`" `"exit`" |findstr guidMasterKey"
     
@@ -17,11 +17,11 @@ Foreach-Object {
     $salida = cmd /c $second_step
     
     $sha1_key = ($salida -split ' ')[3]
-    #write-host $sha1_key
+
 
     #decrypt private key
     $third_step = "mimikatz.exe `"dpapi::capi /in:" + $_.FullName + " /masterkey:" + $sha1_key + "`" `"exit`" "
-    write-host $third_step
+
     $salida = cmd /c $third_step
     write-host $salida
     
