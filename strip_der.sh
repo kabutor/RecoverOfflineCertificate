b1ae794b3945d33c3ea1287b4f5898bf15318e09
kabutor <43006263+kabutor@users.noreply.github.com>
2020-09-19 13:18:35 +0200

    Add files via upload

---
+++ strip_der.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+FILE="./My/Certificates/*"
+OUTDIR='./DER/'
+for file in $FILE 
+do
+	offset=$(binwalk $file|grep 0x|awk '{print $1;exit}')
+	outfile=$(basename $file)
+	dd if=$file of=$OUTDIR$outfile.der bs=1 skip=$offset
+
+done
