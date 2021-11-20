f6b366a25e675f5fcdacfa83f87269b223668c61
kabutor <43006263+kabutor@users.noreply.github.com>
2021-11-20 12:44:16 +0100

    add SID as a variable

--- batchpvkdump.sh
+++ batchpvkdump.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-FILE="./Crypto/RSA/S-1-5-21-3255726888-3229116256-1774431785-1002/*"
+SID='S-1-5-21-2645140126-3212403647-2139448272-1001'
+FILE="./Crypto/RSA/${SID}/*"
 OUTDIR='./PVK/'
 for file in $FILE 
 do
-	python cert_dpapi.py -m=Protect/S-1-5-21-3255726888-3229116256-1774431785-1002/ --sid S-1-5-21-3255726888-3229116256-1774431785-1002 --nopass -f=$file -o $OUTDIR$(basename $file).pem
+	python cert_dpapi.py -m=Protect/$SID/ --sid $SID --nopass -f=$file -o $OUTDIR$(basename $file).pem
 	#outfile=$(basename $file)
 	#dd if=$file of=$OUTDIR$outfile.der bs=1 skip=$offset
 
