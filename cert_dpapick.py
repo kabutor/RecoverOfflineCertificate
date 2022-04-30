b16676f61b20f751b647b4103760c5cc71f868d4
kabutor <43006263+kabutor@users.noreply.github.com>
2022-04-30 20:14:24 +0200

    second draft using dpapick and stripping the public cert

--- cert_dpapick.py
+++ cert_dpapick.py
@@ -2,19 +2,56 @@
 Doing it with DPAPICK3
 
 '''
+import os, sys
 from dpapick3.probes import certificate
 from dpapick3 import blob, masterkey, registry
 
-file = './Crypto/RSA/S-1-5-21-2489364404-1414483991-3321145804-1001/88fc815a745a3310f6d696a6ae12d88d_71d94470-45d0-4d65-b5ff-8fbe5cd68abf'
-masterkey_location = './Protect/S-1-5-21-2489364404-1414483991-3321145804-1001/'
-with open(file, "rb") as f:
-    binary = f.read()
+sid = ''
+password = ''
 
-cert = certificate.PrivateKeyBlob(binary)
-mkp = masterkey.MasterKeyPool()
-mkp.loadDirectory(masterkey_location)
-#print(cert)
-cert.try_decrypt_with_password('XXXXXXXPASSXXXXX',mkp,'S-1-5-21-2489364404-1414483991-3321145804-1001')
-print(cert)
-print("---")
-print(cert.export())
+i = os.scandir('./Crypto/RSA')
+for d in i:
+    if (d.is_dir() and (d.name[0:3] == 'S-1')):
+        sid = str(d.name)
+        print("Found SID : %s " % sid)
+if sid == '':
+    sys.exit('No SID found')
+
+
+print("*" * 80 )
+print("Extracting Public part of the certificate")
+print("*" * 80 )
+
+for pub_cert in os.scandir('./My/Certificates/'):
+    with open(pub_cert.path,'rb') as file:
+        while r := file.read(1):
+            if (r.hex() == '30'):
+                r = file.read(1)
+                if (r.hex() == '82'):
+                    pos = file.tell() -2
+                    print("%s -> found %d" % (pub_cert.name,pos))
+                    file.seek(pos)
+                    fw = open(os.path.join('DER',pub_cert.name + '.der'),'wb')
+                    fw.write(file.read())
+                    fw.close()
+                    break
+masterkey_location = os.path.join('Protect', sid)
+
+for priv_cert in os.scandir(os.path.join('./Crypto/RSA',sid)):
+    with open(priv_cert.path, "rb") as f:
+        binary = f.read()
+        cert = certificate.PrivateKeyBlob(binary)
+        mkp = masterkey.MasterKeyPool()
+        mkp.loadDirectory(masterkey_location)
+        #print(cert)
+        try:
+            cert.try_decrypt_with_password(password,mkp,sid)
+            if (cert.flags.cleartext):
+                print(cert.export())
+                fw = open(os.path.join('PVK',priv_cert.name + '.pem'), 'w')
+                fw.write(cert.export())
+                fw.close()
+        except:
+            print("Not valid decryption or private key")
+        print("-" * 80 )
+
