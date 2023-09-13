e06bc0bb8456b7909c679a16ceafaeef34f1d22b
kabutor <43006263+kabutor@users.noreply.github.com>
2023-09-13 13:17:11 +0200

    Update export_all_64bits.bat

--- export_all_64bits.bat
+++ export_all_64bits.bat
@@ -1,5 +1,6 @@
 REM Place this and Export-ClientCertificates.ps1 into the jailbreak root folder (where the other .bat files are located)
 REM Execute this to export all certficates, each to one file with 12345 as password
+REM you can use later Import-AllPfx.ps1 from this same repo to import all the pfx at once
 
 cd %~dp0\binaries
 @jailbreak64.exe powershell -exec bypass ..\Export-ClientCertificates.ps1 %*
